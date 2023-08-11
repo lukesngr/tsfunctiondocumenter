54ce51a5d91a8503a72e579bc15a231201d072a7
Luke Sanger <lukesngr@gmail.com>
2023-08-11 22:34:28 +1000

    Made code a bit cleaner

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,13 @@ import OpenAI from "./openai/ModelQuerier"
 import * as dotenv from 'dotenv';
 dotenv.config();
 const openAI = new OpenAI("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");
+const testFunction = "function printHello(): void {console.log('Hello!');}"
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "tsfunctiondocumenter" is now active!');
 
 	let disposable = vscode.commands.registerCommand('tsfunctiondocumenter.helloWorld', async () => {
-		await openAI.documentFunction("function printHello(): void {console.log('Hello!');}")
+		await openAI.documentFunction(testFunction)
 		.then(text => {
 			vscode.window.showInformationMessage(text);
 			console.log(text);
