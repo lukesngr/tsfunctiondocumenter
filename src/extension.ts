354634228e0597a05f249209806f7185bcff6c69
Luke Sanger <lukesngr@gmail.com>
2023-08-12 10:17:55 +1000

    Couldn't get dotenv working

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
-import OpenAI from "./ModelQuerier"
-require("dotenv").config();
+import OpenAI from "./ModelQuerier";
 import outputDocToNewEditorTab from './outputDocToNewEditorTab';
 import getCodeFromOpenWindow from './getCodeFromOpenWindow';
 
@@ -8,17 +7,15 @@ const openAI = new OpenAI("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")
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "tsfunctiondocumenter" is now active!');
-	console.log(process.env.APIKEY)
-
 	let disposable = vscode.commands.registerCommand('tsfunctiondocumenter.documentPageFunctions', async () => {
 		let code = getCodeFromOpenWindow();
-		/*await openAI.documentFunction(code)
+		await openAI.documentFunction(code)
 		.then(documentation => {
 			outputDocToNewEditorTab(documentation);
 		})
 		.catch(error => {
 			console.log(error);
-		})*/
+		})
 	});
 
 	context.subscriptions.push(disposable);
