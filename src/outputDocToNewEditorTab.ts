b3b300e74dfe79c43beb9894c08d4e652f146847
JojoRaincy <jojoraincy99@gmail.com>
2023-10-04 12:08:49 +1100

    Update outputDocToNewEditorTab.ts

--- src/outputDocToNewEditorTab.ts
+++ src/outputDocToNewEditorTab.ts
@@ -1,3 +1,8 @@
-export default function outputDocToNewEditorTab(documentation: String): void {
-    console.log(documentation);
+import * as vscode from 'vscode';
+
+export default function outputDocToNewEditorTab(documentation: string): void {
+    const doc = vscode.workspace.openTextDocument({ content: documentation, language: 'javascript' });
+    doc.then(document => {
+        vscode.window.showTextDocument(document);
+    });
 }
