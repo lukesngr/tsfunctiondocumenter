d1fee6a4554d62c9d33a1b1b9f55ed62544206d7
JojoRaincy <jojoraincy99@gmail.com>
2023-10-04 12:08:30 +1100

    Update getCodeFromOpenWindow.ts

--- src/getCodeFromOpenWindow.ts
+++ src/getCodeFromOpenWindow.ts
@@ -1,3 +1,13 @@
-export default function getCodeFromOpenWindow(): string {
-    return "function printHello(): void {console.log('Hello!');}";
+import * as vscode from 'vscode';
+
+export default function getCodeFromOpenWindow(): string | null {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return null;
+    }
+
+    const selection = editor.selection;
+    const text = selection.isEmpty ? editor.document.getText() : editor.document.getText(selection);
+
+    return text;
 }
