589a2796c2a096da431f1f1ffb1cc8a3bee7fc33
Luke Sanger <lukesngr@gmail.com>
2023-10-15 11:58:23 +1100

    API Key settings made

--- src/extension.ts
+++ src/extension.ts
@@ -3,22 +3,31 @@ import OpenAI from "./ModelQuerier";
 import outputDocToNewEditorTab from './outputDocToNewEditorTab';
 import getCodeFromOpenWindow from './getCodeFromOpenWindow';
 
-const openAI = new OpenAI("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");
-
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "tsfunctiondocumenter" is now active!');
-	let disposable = vscode.commands.registerCommand('tsfunctiondocumenter.documentPageFunctions', async () => {
-		let code = getCodeFromOpenWindow();
-		await openAI.documentFunction(code)
-		.then(documentation => {
-			outputDocToNewEditorTab(documentation);
-		})
-		.catch(error => {
-			console.log(error);
-		})
-	});
+    console.log('Congratulations, your extension "tsfunctiondocumenter" is now active!');
 
-	context.subscriptions.push(disposable);
+    let config = vscode.workspace.getConfiguration('tsfunctiondocumenter');
+    let apiKey: string | undefined = config.apiKey;
+    
+    if (!apiKey) {
+        vscode.window.showErrorMessage("API Key for tsfunctiondocumenter is not set!");
+        return;
+    }
+
+    const openAI = new OpenAI(apiKey);
+
+    let disposable = vscode.commands.registerCommand('tsfunctiondocumenter.documentPageFunctions', async () => {
+        try {
+            let code = getCodeFromOpenWindow();
+            const documentation = await openAI.documentFunction(code);
+            outputDocToNewEditorTab(documentation);
+        } catch (error) {
+            console.error(error);
+            vscode.window.showErrorMessage("An error occurred while documenting the functions.");
+        }
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
